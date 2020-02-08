2b70f491ece56d1eb485c479a1ea0121e15dccbc
encrypticus <encrypticus@gmail.com>
2020-02-08 22:43:32 +0600

    Рефакторинг кода редьюсера.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,6 +6,46 @@ const initialState = {
   orderTotal: 220
 };
 
+const updateCartItems = (cartItems, item, index) => {
+  if (index < 0) {
+
+    return [
+      ...cartItems,
+      item
+    ];
+
+  } else {
+
+    return [
+      ...cartItems.slice(0, index),
+      item,
+      ...cartItems.slice(index + 1)
+    ];
+
+  }
+};
+
+const updateCartItem = (book, item) => {
+  if (item) {
+
+    return {
+      ...item,
+      count: item.count + 1,
+      total: item.total + book.price
+    };
+
+  } else {
+
+    return {
+      id: book.id,
+      title: book.title,
+      count: 1,
+      total: book.price
+    };
+
+  }
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
 
@@ -38,41 +78,12 @@ const reducer = (state = initialState, action) => {
       const itemIndex = state.cartItems.findIndex(item => item.id === bookId);
       const item = state.cartItems[itemIndex];
 
-      let newItem;
+      let newItem = updateCartItem(book, item);
 
-      if (item) {
-        newItem = {
-          ...item,
-          count: item.count + 1,
-          total: item.total + book.price
-        }
-      } else {
-        newItem = {
-          id: book.id,
-          title: book.title,
-          count: 1,
-          total: book.price
-        };
-      }
-
-      if (itemIndex < 0) {
-        return {
-          ...state,
-          cartItems: [
-            ...state.cartItems,
-            newItem
-          ]
-        };
-      } else {
-        return {
-          ...state,
-          cartItems: [
-            ...state.cartItems.slice(0, itemIndex),
-            newItem,
-            ...state.cartItems.slice(itemIndex + 1)
-          ]
-        }
-      }
+      return {
+        ...state,
+        cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
+      };
 
 
     default:
